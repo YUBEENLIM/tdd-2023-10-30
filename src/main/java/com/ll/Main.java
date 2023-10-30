89840ea8b308cc76b8db9c0d0b21d47dd728b28c
YUBEENLIM <bgs0782@gmail.com>
2023-10-30 09:27:54 +0900

    초기상태

---
+++ src/main/java/com/ll/Main.java
@@ -0,0 +1,13 @@
+package com.ll;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Calc.run("3*1") == 3;
+        System.out.println("Hello world!");
+    }
+}
+
+public class Calc{
+    Calc =
+}
